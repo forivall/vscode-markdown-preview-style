d38fad8e843503b21f8b5529cdf667e8fa200a83
Emily Marigold Klassen <forivall@users.noreply.github.com>
2020-10-01 20:39:46 -0700

    feat: add dark css theme

--- build/update-github-markdown-css.js
+++ build/update-github-markdown-css.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
+const path = require('path');
+const fs = require('fs');
+
+const generateDarkCss = require('generate-github-markdown-css-dark');
 
 const inputPath = path.join('node_modules', 'github-markdown-css', 'github-markdown.css');
 const fullPath = path.join(__dirname, '..', inputPath);
@@ -17,11 +20,36 @@ function updateGithubMarkdownCss(input) {
                 px === "16" ? `/* font-size: 1em${semi} */` : `font-size: ${Number(px) / 16}em${semi}`
             )
     );
-} 
+}
 
+main();
+async function main() {
+    const input = fs.readFileSync(fullPath, 'utf8');
 
-const input = fs.readFileSync(fullPath, 'utf8');
+    fs.writeFileSync(
+        path.join(__dirname, '..', 'github-markdown.css'),
+        updateGithubMarkdownCss(input));
 
-fs.writeFileSync(
-    path.join(__dirname, '..', 'github-markdown.css'),
-    updateGithubMarkdownCss(input));
+    const darkStyle = process.argv[2] || 'https://github.com/DarkThemeHub/GithubDarkTheme/raw/master/Generated/github.user.styl';
+
+    const darkInput = await generateDarkCss({ darkStyle });
+
+    // other fixes needed, generally to pull the styles applied to the body:
+    // https://github.com/StylishThemes/GitHub-Dark/raw/master/github-dark.user.css
+    // https://github.com/neyasbltb88/github-dark-theme/raw/master/style.css
+    // https://github.com/cquanu/github-dark/raw/master/github-dark.user.css
+
+    // TODO: these fixups should be in base.css instead
+    const extra = darkStyle.includes('DarkThemeHub/GithubDarkTheme') ? `
+html {
+  background-color: #222 !important;
+}
+.vscode-body {
+  background: #25272a !important;
+}
+` : ''
+
+    fs.writeFileSync(
+        path.join(__dirname, '..', 'github-markdown-dark.css'),
+        updateGithubMarkdownCss(darkInput) + extra);
+}
